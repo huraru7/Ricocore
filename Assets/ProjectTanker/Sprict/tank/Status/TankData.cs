9be14da8e5f3c2841dcf69bdbb55852650760626
huraru <hurarunium@gmail.com>
2026-04-24 21:50:22 +0900

    [Add]初期設定をScrObjで現在値をMonoBehaviourで管理

---
+++ Assets/ProjectTanker/Sprict/tank/Status/TankData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Create TankData")]
+public class TankData
+{
+
+}
---
+++ Assets/ProjectTanker/Sprict/tank/Status/TankStatus.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using UnityEngine;
+
+public class TankStatus : MonoBehaviour
+{
+    [SerializeField] private TankData data;
+
+}
