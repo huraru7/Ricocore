b9c59bd50684ea1f3b1001ed7296a91ad70fab2a
huraru <hurarunium@gmail.com>
2026-04-24 22:11:54 +0900

    [Updata]

--- Assets/ProjectTanker/Sprict/tank/Status/TankData.cs
+++ Assets/ProjectTanker/Sprict/tank/Status/TankData.cs
@@ -1,8 +1,11 @@
 using System.Collections;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Data/Create TankData")]
-public class TankData
+public class TankData : ScriptableObject
 {
-
+    [Tooltip("最大HP")] public int maxHP;
+    [Tooltip("移動速度")] public int movementSpeed;
+    [Tooltip("旋回速度")] public int turnRate;
 }
--- Assets/ProjectTanker/Sprict/tank/Status/TankStatus.cs
+++ Assets/ProjectTanker/Sprict/tank/Status/TankStatus.cs
@@ -5,4 +5,14 @@ public class TankStatus : MonoBehaviour
 {
     [SerializeField] private TankData data;
 
+    public int HP { get; private set; }
+    public int movementSpeed { get; private set; }
+    public int turnRate { get; private set; }
+
+    void Start()
+    {
+        HP = data.maxHP;
+        movementSpeed = data.movementSpeed;
+        turnRate = data.turnRate;
+    }
 }
